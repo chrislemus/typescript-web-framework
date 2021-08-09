2e776ca5b68e1d866ad14110c5ffcd0e485f164f
chrislemus <icrislemus@gmail.com>
2021-08-09 14:25:17 -0400

    add save/fetch methods to User class

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,4 @@
 import { User } from './models/User';
 
-const user = new User({ name: 'myname', age: 20 });
-user.on('change', () => {
-  console.log('object');
-});
-user.on('change', () => {
-  console.log('second');
-});
-
-user.trigger('dew');
-
-console.log(user);
-// user.set({ name: 'newName' });
-
-// console.log(user.get('name'));
+const user = new User({ name: 'new record', age: 0 });
+user.save();
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,7 @@
+import axios, { AxiosResponse } from 'axios';
+
 interface UserProps {
+  id?: number;
   name?: string;
   age?: number;
 }
@@ -28,5 +31,23 @@ export class User {
     const handlers = this.events[eventName];
     if (!handlers || handlers.length === 0) return;
     handlers.forEach((callback) => callback());
+  }
+
+  fetch(): void {
+    const id = this.get('id');
+    axios
+      .get(`http://localhost:3000/users/${id}`)
+      .then((res: AxiosResponse): void => {
+        this.set(res.data);
+      });
+  }
+
+  save(): void {
+    const id = this.get('id');
+    if (id) {
+      axios.put(`http://localhost:3000/users/${id}`, this.data);
+    } else {
+      axios.post('http://localhost:3000/users', this.data);
+    }
   }
 }
